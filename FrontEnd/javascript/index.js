b8ee1399b50809b23584f51eb9ed642b6fe064e1
harim1206 <harim1206@gmail.com>
2018-05-01 17:34:11 -0400

    final

--- FrontEnd/javascript/index.js
+++ FrontEnd/javascript/index.js
@@ -4,70 +4,230 @@
 // Allow users to create, edit and delete notes.
 // Feel free to add on your own features if you have built all of the above! Some ideas: You could add filter or search functionality, multiple users, or support for rich format (bold, italic, etc) when creating a note.
 
+let sideBarDiv = document.getElementById('side-bar')
+let mainDiv = document.getElementById('main')
+let newNoteButton = document.getElementById('newNoteButton')
+let newNoteForm = document.getElementById('newForm')
+
 
 // Listing all of a user's notes on a sidebar --> For now, only create one user. There will be no log in.
 fetch('http://localhost:3000/api/v1/notes')
   .then((res) => res.json())
   .then((json) => {
-    debugger
-    // json
     let noteTitles = json.map((note)=>note.title)
-    let sideBarDiv = document.getElementById('side-bar')
-    let mainDiv = document.getElementById('main')
-
 
     json.forEach((note)=>{
-      let titleWrapperDiv = document.createElement('div')
-      titleWrapperDiv.innerHTML = note.title
-      titleWrapperDiv.className = 'noteTitle'
+      handleNote(note)
+    })
 
-      sideBarDiv.appendChild(titleWrapperDiv)
+  })
 
+newNoteButton.addEventListener('click',(e)=>{
+  if(document.getElementsByClassName('noteWrapperP').length>0){
+    document.querySelector('.noteWrapperP').remove()
+    document.querySelector('#deleteButton').remove()
+  }
+  newNoteButton.disabled = true
 
-      titleWrapperDiv.addEventListener('click',function(e){
-        // let notes = document.querySelectorAll('.noteWrapperP')
-        // debugger;
+  createForm()
 
-        // if (notes.find(el => (el.style.display === 'block'))) {
-        //   let displayed = notes.find(el => (el.style.display === 'block'))
-        //   displayed.style.display = 'none'
-        // }
-        // noteWrapperP.parentNode.firstChild.style.display = "none"
+  let newForm = document.getElementById('newForm')
 
-        // debugger
-        if(document.getElementsByClassName('noteWrapperP').length>0){
-          document.querySelector('.noteWrapperP').remove()
-        }
-
-        let noteWrapperP = document.createElement('p')
-        noteWrapperP.innerHTML = note.body
-        noteWrapperP.className = 'noteWrapperP'
-        noteWrapperP.id = note.title
-        noteWrapperP.style.display = 'none'
-        noteWrapperP.style.display = "block"
+  newForm.addEventListener('submit',(e)=>{
+    newNoteButton.disabled = false
+    e.preventDefault()
 
 
-        mainDiv.appendChild(noteWrapperP)
+    let titleInput = document.getElementById('titleInput')
+    let noteBodyInput = document.getElementById('noteBodyInput')
+
+    // debugger
 
 
+    let postData = {
+      title: titleInput.value,
+      body: noteBodyInput.value
+    }
+
+    fetch('http://localhost:3000/api/v1/notes', {
+        method: 'POST',
+          body: JSON.stringify(postData),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+      }).then(res => {
+        handleNote(postData)
+        newForm.remove()
       })
 
-      // debugger
 
 
+
+  })
+
+})
+
+
+function handleNote(note){
+  // debugger
+
+  let titleWrapperDiv = document.createElement('div')
+  titleWrapperDiv.innerHTML = note.title
+  titleWrapperDiv.className = 'noteTitle'
+  titleWrapperDiv.id = note.id
+
+  sideBarDiv.appendChild(titleWrapperDiv)
+
+  titleWrapperDiv.addEventListener('click',function(e){
+    if(document.getElementsByClassName('noteWrapperP').length>0){
+
+      document.querySelector('.noteWrapperP').remove()
+      document.querySelector('#deleteButton').remove()
+      document.querySelector('#editButton').remove()
+    }
+
+    if(document.querySelector('#newForm')){
+      document.querySelector('#newForm').remove()
+      document.querySelector('#newNoteButton').disabled = false
+
+    }
+
+    let noteWrapperP = document.createElement('p')
+    noteWrapperP.innerHTML = note.body
+    noteWrapperP.className = 'noteWrapperP'
+    noteWrapperP.id = note.title
+    noteWrapperP.style.display = 'none'
+    noteWrapperP.style.display = "block"
+
+    let deleteButton = document.createElement('button')
+    deleteButton.id = "deleteButton"
+    deleteButton.innerHTML = "Delete"
+    deleteButton.addEventListener('click', ()=> {
+      deleteNote(titleWrapperDiv, noteWrapperP, deleteButton, editButton, note.id)
+    })
+
+    let editButton = document.createElement('button')
+    editButton.id = "editButton"
+    editButton.innerHTML = "Edit"
+    editButton.addEventListener('click', ()=> {
+      // console.log("in EDIT")
+
+      editNote(note.id)
     })
 
 
 
 
 
-    // debugger
+
+    mainDiv.appendChild(noteWrapperP)
+    mainDiv.appendChild(editButton)
+    mainDiv.appendChild(deleteButton)
   })
 
+}
+
+
+function deleteNote(title, body,
+  deleteButton, editButton, id) {
+  // debugger
+  fetch('http://localhost:3000/api/v1/notes/' + id, {
+      method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+    }).then(res => {
+      body.remove()
+      title.remove()
+      deleteButton.remove()
+      editButton.remove()
+
+
+    })
+}
 
 
 
-// debugger
-// fetch('https://api.github.com/repos/jquery/jquery/commits')
-//   .then(res => res.json())
-//   .then(json => console.log(json));
+function editNote(id){
+  let editButton = document.getElementById('editButton')
+  let deleteButton = document.getElementById('deleteButton')
+  editButton.style.display = 'none'
+  deleteButton.style.display = 'none'
+
+  createForm()
+
+
+
+  let newForm = document.getElementById('newForm')
+  newForm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    let titleInput = document.getElementById('titleInput')
+    let noteBodyInput = document.getElementById('noteBodyInput')
+    let postData = {
+      title: titleInput.value,
+      body: noteBodyInput.value
+    }
+
+    let previousTitle = document.getElementById(id)
+    let previousBody = document.getElementById(previousTitle.innerHTML)
+    previousTitle.remove()
+    previousBody.remove()
+
+
+    fetch('http://localhost:3000/api/v1/notes/' + id, {
+        method: 'PATCH',
+          body: JSON.stringify(postData),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+      }).then(res => {
+
+
+
+        handleNote(postData)
+        newForm.remove()
+      })
+
+
+
+
+  })
+
+}
+
+function createForm(){
+
+    let newForm = document.createElement('form')
+    newForm.id = 'newForm'
+
+    let titleLabel = document.createElement('label')
+    let titleInput = document.createElement('input')
+    let noteBodyLabel = document.createElement('label')
+    let noteBodyInput = document.createElement('textarea')
+    let submitButton = document.createElement('button')
+
+    titleInput.setAttribute('id', 'titleInput')
+    noteBodyInput.setAttribute('id', 'noteBodyInput')
+
+    titleLabel.setAttribute('for', 'titleInput')
+    titleLabel.innerHTML = "title"
+    noteBodyLabel.setAttribute('for', 'noteInput')
+    noteBodyLabel.innerHTML = "body"
+
+    submitButton.setAttribute('type','submit')
+    // submitButton.setAttribute('value', 'Create Note')
+    submitButton.innerHTML = 'Submit'
+
+
+    newForm.appendChild(titleLabel)
+    newForm.innerHTML += '<br>'
+    newForm.appendChild(titleInput)
+    newForm.innerHTML += '<br>'
+    newForm.appendChild(noteBodyLabel)
+    newForm.innerHTML += '<br>'
+    newForm.appendChild(noteBodyInput)
+    newForm.innerHTML += '<br>'
+    newForm.appendChild(submitButton)
+    mainDiv.appendChild(newForm)
+
+}
